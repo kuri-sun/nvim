cd15a2b15d07d79678ce66aa83b846b39e69e46b
kuri-sun <hello@haruki-k.org>
2025-09-28 01:01:02 +0900

    chore: clean up options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,20 +1,13 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- NOTE: default options: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- NOTE: Non written options will be applied before lazy.nvim startup
+-- vim.opt.hlsearch = true -- Highlights all matches for the last search pattern.
+-- vim.opt.incsearch = true -- Shows partial matches for a search as you type
+-- vim.opt.ignorecase = true -- Ignores case in search patterns.
+-- vim.opt.autoindent = true -- Automatically indents a new line to the same level as the previous line.
+-- vim.opt.backspace = { "start", "eol", "indent" } -- Defines how the <Backspace> key works to delete across line boundaries, at the start of an insertion, and to delete auto-indentation.
 
-vim.opt.title = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.termguicolors = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.scrolloff = 10
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.autoindent = true
-vim.opt.backspace = { "start", "eol", "indent" }
+-- keep cursor in the middle of the screen
+vim.opt.scrolloff = 8;
 
-vim.g.mapleader = " "
+-- disable autoformatting
+vim.g.autoformat = false
